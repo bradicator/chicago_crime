968a47243a77295dcbf74e96f29e771adfae5e98
Brad <bradzhao@mbp.local>
2023-11-19 11:40:18 -0600

    first working release

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# chicago_crime
+# Chicago_crime
 Some python code to analyze chicago crime statistics by looking at police records 
 
 The city of Chicago provides reported incidents of crime since 2001 (except murder) [here](https://data.cityofchicago.org/Public-Safety/Crimes-2001-to-Present/ijzp-q8t2). I use 2022 and 2023's data for this analysis.
@@ -6,3 +6,8 @@ The city of Chicago provides reported incidents of crime since 2001 (except murd
 Another useful link is [this](https://data.cityofchicago.org/Public-Safety/Boundaries-Police-Beats-current-/aerh-rz74), which gives a map of police districts and beats.
 
 An explanation of crime code used in the data is [here](https://gis.chicagopolice.org/pages/crime_details)
+
+# Usage
+Drop a pin on google map to get the (latitude, longitude) of the location of interest. Call `CrimeAnalyzer.select_area(latitude, longitude, radius)` to lock in the area. Then call `CrimeAnalyzer.get_report` to see the local report. See `example.ipynb` for an example
+
+![pin](./google_map_pin.png)
--- utils.py
+++ utils.py
@@ -1,21 +1,9 @@
 import numpy as _np
 import pandas as _pd
+import seaborn as _sns
+import matplotlib.pyplot as _plt
 
 from collections import defaultdict as _defaultdict
-
-default_values = {
-    '01A': 'homicide_violent',
-    '03': 'robbery_violent',
-    '04A': 'aggravated_assault_violent',
-    '04B': 'aggravated_battery_violent',
-    '05': 'burglary_nonviolent',
-    '06': 'theft_nonviolent',
-    '07': 'car_theft_nonviolent',
-    '08A': 'simple_assault_violent',
-    '08B': 'simple_battery_violent'
-}
-uci2name = _defaultdict(str, default_values)
-
 def get_distance(longitudes, latitudes, ref_latitude, ref_longitude, dist_type='l1'):
     longitudes = _np.asarray(longitudes)
     latitudes = _np.asarray(latitudes)
@@ -33,16 +21,28 @@ def get_distance(longitudes, latitudes, ref_latitude, ref_longitude, dist_type='
 class CrimeAnalyzer:
     default_values = {
         '01A': 'homicide_violent',
+        '01B': 'manslaughter_violent',
+        '02': 'sexual_assault_violent',
         '03': 'robbery_violent',
         '04A': 'aggravated_assault_violent',
         '04B': 'aggravated_battery_violent',
-        '05': 'burglary_burglary',
-        '06': 'theft_theft',
-        '07': 'car_theft_theft',
+        '05': 'burglary_nonviolent',
+        '06': 'theft_nonviolent',
+        '07': 'car_theft_nonviolent',
         '08A': 'simple_assault_violent',
-        '08B': 'simple_battery_violent'
+        '08B': 'simple_battery_violent',
+        '10': 'forgery_nonviolent',
+        '11': 'fraud_nonviolent',
+        '12': 'embezzlement_nonviolent',
+        '13': 'stolen_property_nonviolent',
+        '14': 'vandalism_nonviolent',
+        '15': 'weapon_violation_violent',
+        '16': 'prostitution_nonviolent',
+        '17': 'sex_abuse_nonviolent',
+        '18': 'drug_abuse_nonviolent',
+        '24': 'disorderly_conduct_nonviolent',
     }
-    uci2name = _defaultdict(str, default_values)
+    uci2name = _defaultdict(lambda : "uncategorized_nonviolent", default_values)
 
     def __init__(self, data_files) -> None:
         if isinstance(data_files, str):
@@ -52,7 +52,7 @@ class CrimeAnalyzer:
             for file in data_files:
                 temp.append(_pd.read_csv(file))
                 self.rawdf = _pd.concat(temp, axis=0)
-        self.rawdf['FBI Code'] = self.rawdf['FBI Code'].apply(lambda x: uci2name[x])
+        self.rawdf['FBI Code'] = self.rawdf['FBI Code'].apply(lambda x: CrimeAnalyzer.uci2name[x])
         self.rawdf = self.rawdf.dropna(subset=['Latitude'])
         columns_to_select = ['Case Number', 'Date', 'Primary Type',
        'Description', 'Location Description', 'Arrest', 'Domestic', 'Beat',
@@ -76,7 +76,84 @@ class CrimeAnalyzer:
         self.rawdf = self.rawdf[columns_to_select].rename(columns=column_mapping)
         self.rawdf['ts'] = _pd.to_datetime(self.rawdf['ts'])
         self.rawdf['violent'] = self.rawdf.category.apply(lambda x: x.split("_")[-1])
+        self.rawdf.category = self.rawdf.category.apply(lambda x: x[:x.rfind("_")])
+        self.clear_selection()
     
-    def select_area(self, longitude, latitude, distance=500, distance_type="l1"):
-        distances = get_distance(self.rawdf.longitude, self.rawdf.latitude, longitude, latitude, distance_type)
+    def select_area(self, latitude, longitude, distance=500, distance_type="l1"):
+        """
+        specify latitude first because google map gives latitude first
+        """
+        self.clear_selection()
+        distances = get_distance(self.rawdf.longitude, self.rawdf.latitude, latitude, longitude, distance_type)
         self.focusdf = self.rawdf.loc[distances < distance, :].copy()
+        self.focusdf['year_quarter'] = self.focusdf.ts.dt.year.astype(str) + 'Q' + self.focusdf.ts.dt.quarter.astype(str)
+        self.focus_radius = distance
+        self.focus_center = (longitude, latitude)
+    
+    def clear_selection(self):
+        self.focusdf = None
+        self.focus_center = None
+    
+    def get_summary_count(self):
+        if self.focusdf is None:
+            raise ValueError("must select the area of interest.")
+        vvc = self.focusdf.violent.value_counts()
+            # vvc_str = f"This area has a total of {vvc.sum()} crimes, with {vvc['violent']} violent ones, {vvc['nonviolent']} non-violent ones, and {vvc['uncategorized']} uncategorized ones."
+        cvc = self.focusdf.category.value_counts()
+        return vvc, cvc
+    
+    def get_hourly_plot(self, by_violence=True):
+        if self.focusdf is None:
+            raise ValueError("must select the area of interest.")
+        f, ax = _plt.subplots(figsize=(8, 6))
+        if by_violence:
+            countdf = self.focusdf.groupby(['violent', self.focusdf.ts.dt.hour])['category'].count().reset_index()
+            bar_plot = _sns.barplot(x=countdf.ts, y=countdf.category, hue=countdf.violent, ax=ax)
+        else:
+            countdf = self.focusdf.groupby([self.focusdf.ts.dt.hour])['category'].count().reset_index()
+            bar_plot = _sns.barplot(x=countdf.ts, y=countdf.category, ax=ax)
+        ax.set(ylabel="Count", xlabel="Crime By Hour of Day")
+        return f
+    
+    def get_density_plot(self):
+        if self.focusdf is None:
+            raise ValueError("must select the area of interest.")
+        joint_plot = _sns.jointplot(x=self.focusdf.longitude, y=self.focusdf.latitude, 
+                                    hue=self.focusdf.violent, marker="+", s=25, height=6)
+        if self.focus_center:
+            joint_plot.ax_joint.scatter(*self.focus_center, marker='*', s=200)
+        joint_plot.ax_joint.set(ylabel="Latitude", xlabel="Longitude")
+        joint_plot.fig.suptitle("Crime Density Map")
+        return joint_plot
+    
+    def get_quarterly_plot(self, by_violence=True):
+        if self.focusdf is None:
+            raise ValueError("must select the area of interest.")
+        f, ax = _plt.subplots(figsize=(8, 6))
+        if by_violence:
+            countdf = self.focusdf.groupby(['year_quarter','violent'])['category'].count().reset_index()
+            bar_plot = _sns.barplot(x=countdf.year_quarter, y=countdf.category, hue=countdf.violent, ax=ax)
+        else:
+            countdf = self.focusdf.groupby(['year_quarter'])['category'].count().reset_index()
+            bar_plot = _sns.barplot(x=countdf.year_quarter, y=countdf.category, ax=ax)
+        ax.set(ylabel="Count", xlabel="Crime by Quarter")
+        return f
+        
+    def get_report(self, by_violence=True, address=None):
+        if self.focusdf is None:
+            raise ValueError("must select the area of interest.")
+        if address is None:
+            address = f"coordinate ({self.focus_center[0]:.3f}, {self.focus_center[1]:.3f})"
+        vvc, cvc = self.get_summary_count()
+        print(f"Found {vvc.sum()} crimes in total for area within {self.focus_radius}m of "+address+":")
+        print("====================================================")
+        print("Crime Count By Violence:\n", vvc[:5].to_csv(header=False))
+        print("Top Crime Types:\n", cvc[:5].to_csv(header=False))
+        _ = self.get_hourly_plot(by_violence)
+        self.get_density_plot()
+        _ = self.get_quarterly_plot(by_violence)
+        return 
+
+        
+
+    
